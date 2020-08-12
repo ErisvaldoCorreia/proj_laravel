3b3197c02535cc2b0d01cc423b80c0226c88dc28
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2020-08-12 12:28:43 -0300

    Seed para Heroku

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(ProdutoTableSeeder::class);
+    }
+}
+
+class ProdutoTableSeeder extends Seeder {
+    public function run()
+    {
+        DB::insert('INSERT INTO users (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`)
+        VALUES (?,?,?,?,?,?,?,?)',
+        array('2', 'Empresa', 'empresa@empresa.com', '2020-08-12 12:18:31', '12345', '12345', '2020-08-12 12:18:31', '2020-08-12 12:18:31'));
+
+        DB::insert('INSERT INTO users (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`)
+        VALUES (?,?,?,?,?,?,?,?)',
+        array('3', 'Amizade', 'empresa@empresa.com', '2020-08-12 12:18:31', '12345', '12345', '2020-08-12 12:18:31', '2020-08-12 12:18:31'));
     }
 }
